d7671729c5a6c9ea5b1391f1c194790b15400ea9
shivam <srvcode@gmail.com>
2021-03-12 08:04:26 -0800

    Adding Code for @Value @Autowired @Qualifier @Component

---
+++ spring-demo-annotations/src/com/srvcode/springdemo/RESTFortuneService.java
@@ -0,0 +1,13 @@
+package com.srvcode.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
--- spring-demo-annotations/src/com/srvcode/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/srvcode/springdemo/TennisCoach.java
@@ -1,15 +1,60 @@
 package com.srvcode.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	// Field Injection
+	/*
+	 * Since there are multiple FortuneService implementations
+	 * we have to use @Qualifier to provide specific implementation
+	 */
+	//@Autowired
+	//@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define a setter method
+	
+	/*
+	 * Setter any method Injection
+	 * 
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+		fortuneService = theFortuneService;
+	}
+	 */
+	
+	/*
+	 * Setter Injection
+	 * 
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
+	 * Constructor Injection
+	 * 
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	@Autowired
+	public TennisCoach(@Qualifier("happyFortuneService") FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside constructor using @Autowired & @Qualifier");
 		fortuneService = theFortuneService;
 	}
 	
