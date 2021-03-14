2d6b18a14203e1490070e829691ed8fdb89be58a
shivam <srvcode@gmail.com>
2021-03-14 09:23:30 -0700

    Adding code for @PostConstruct & @PreDestroy annotations

--- spring-demo-annotations/src/com/srvcode/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/srvcode/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.srvcode.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	// Field Injection
@@ -14,13 +17,26 @@ public class TennisCoach implements Coach {
 	 * Since there are multiple FortuneService implementations
 	 * we have to use @Qualifier to provide specific implementation
 	 */
-	//@Autowired
-	//@Qualifier("happyFortuneService")
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TenisCoach: inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
 	}
 	
 	// define a setter method
@@ -54,12 +70,15 @@ public class TennisCoach implements Coach {
 	}
 	*/
 	
+	/*
+	 * Constructor Injection with Qualifier
+	 * 
 	@Autowired
 	public TennisCoach(@Qualifier("happyFortuneService") FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside constructor using @Autowired & @Qualifier");
 		fortuneService = theFortuneService;
 	}
-	
+	*/
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
