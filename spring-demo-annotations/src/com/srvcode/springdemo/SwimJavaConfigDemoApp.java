673de4b6b6f452a235d394d3e8d4668ce4a934e5
shivam <srvcode@gmail.com>
2021-03-19 08:50:32 -0700

    Adding Code for @PropertySource annotation

--- spring-demo-annotations/src/com/srvcode/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/srvcode/springdemo/SwimJavaConfigDemoApp.java
@@ -10,12 +10,16 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call our new swim coach methods ... has the props values injected
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
 		
 		// close the context
 		context.close();
